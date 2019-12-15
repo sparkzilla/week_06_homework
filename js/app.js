e057216ceee199ba76b2d3438ac1982f04bdb174
user <user@users-MacBook-Pro-9.local>
2019-12-15 14:09:43 +0000

    Added delete button

---
+++ js/app.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('JavaScript loaded');
+
+  //FORM
+    const formElement = document.querySelector('#new-item-form');
+    const handleFormSubmit = function(event){
+      event.preventDefault();
+
+      const newPlace = document.createElement('li');
+      newPlace.textContent = `${event.target.sight.value} ${event.target.city.value} ${event.target.status.value}`;
+      newPlace.classList.add('list');
+      const list = document.querySelector('#places-list')
+      list.appendChild(newPlace);
+
+      document.getElementById('new-item-form').reset();
+    }
+
+    formElement.addEventListener('submit', handleFormSubmit);
+
+    const buttonElement = document.querySelector('#delete-all');
+    const handleButtonClick = function() {
+    const list = document.querySelector('#places-list');
+    list.innerHTML  = '';
+    }
+
+    buttonElement.addEventListener('click', handleButtonClick);
+
+})
